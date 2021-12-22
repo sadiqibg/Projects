6373be06d4731a7c154169493776aee430aa288a
Abubakar <sadiqibg@gmail.com>
2021-12-22 16:18:19 -0500

    Testing in Progress

--- Tic Tac Toe/test_TicTacToe.py
+++ Tic Tac Toe/test_TicTacToe.py
@@ -4,7 +4,20 @@ The def
 
 """
 
+import unittest
+from unittest.case import TestCase 
+import methods
+import TicTacToe
+import config
 
 
-def test():
-    pass
+class TestMethods(unittest, TestCase):
+    
+    def test_place_marker(self):
+        # Placing marker in spot 1
+        methods.place_marker(1)
+        self.assertNotEqual(config.label1.cget("text"),"")
+
+
+if __name__ == '__main__':
+    unittest.main()
