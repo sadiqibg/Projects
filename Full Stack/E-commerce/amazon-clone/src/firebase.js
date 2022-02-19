361614844d0f950a845d8c9d2aa1f0c18a49e541
Abubakar <sadiqibg@gmail.com>
2022-02-19 11:10:50 -0500

    new app

---
+++ Full Stack/E-commerce/amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBWsINIjQdoMnFJTKjnSW09RVHX5pWHZxo",
+    authDomain: "challenge-b70f6.firebaseapp.com",
+    projectId: "challenge-b70f6",
+    storageBucket: "challenge-b70f6.appspot.com",
+    messagingSenderId: "928635420550",
+    appId: "1:928635420550:web:ef5be7b99909d1ee8fb607",
+    measurementId: "G-NV34MLLH2C"
+  };
